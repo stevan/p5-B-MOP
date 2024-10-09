e06989a36c37c33a6a859d77b5de235655176b3b
Stevan Little <stevan.little@gmail.com>
2024-10-09 21:26:56 +0200

    stuff

--- lib/B/MOP/Tools/PropogateTypeInformation.pm
+++ lib/B/MOP/Tools/PropogateTypeInformation.pm
@@ -31,6 +31,59 @@ class B::MOP::Tools::PropogateTypeInformation {
         $node->set_type( $node->expression->get_type );
     }
 
+    method visit_numeric_op ($node) {
+        my $node_type = $node->get_type;
+
+        my $lhs_type  = $node->lhs->get_type;
+        my $rhs_type  = $node->rhs->get_type;
+
+        say ">> $node : node($node_type) := lhs($lhs_type) <op> rhs($rhs_type)";
+
+        if ($lhs_type->is_same_type($rhs_type)) {
+            say ">>>> lhs($lhs_type) == rhs($rhs_type)";
+            if ($node_type->is_same_type($lhs_type)) {
+                say "<<<< node($node_type) == lhs($lhs_type) == rhs($rhs_type)";
+            }
+            elsif ($lhs_type->can_downcast_to($node_type)) {
+                say "//// node($node_type) == lhs($lhs_type) == rhs($rhs_type)";
+                $node->lhs->set_type($lhs_type->cast($node_type));
+                $node->rhs->set_type($rhs_type->cast($node_type));
+
+                $lhs_type  = $node->lhs->get_type;
+                $rhs_type  = $node->rhs->get_type;
+
+                say "|||| node($node_type) == lhs($lhs_type) == rhs($rhs_type)";
+            }
+            else {
+                say "<<<< node($node_type) != lhs($lhs_type) == rhs($rhs_type)";
+                if (my $new_type = $node_type->cast($lhs_type)) {
+                    $node->set_type($new_type);
+                }
+                else {
+                    die "TYPE ERROR: Cannot cast $node_type to $lhs_type"
+                        unless $node_type->is_same_type($lhs_type);
+                }
+            }
+        }
+        else {
+            my $rel = $lhs_type->compare($rhs_type);
+            if (!defined $rel) {
+                say ">>>> lhs($lhs_type) != rhs($rhs_type)";
+                if ($lhs_type->can_cast_to($node_type) && $rhs_type->can_cast_to($node_type)) {
+                    say ">><< $node : lhs($lhs_type) > node($node_type) && rhs($rhs_type) > node($node_type)";
+                    $node->lhs->set_type($lhs_type->cast($node_type));
+                    $node->rhs->set_type($rhs_type->cast($node_type));
+                }
+                else {
+                    die "TYPE ERROR: Cannot resolve $lhs_type and $rhs_type with $node_type";
+                }
+            }
+            else {
+                say ">>>> $node : lhs($lhs_type) <> rhs($rhs_type)";
+            }
+        }
+    }
+
     method visit_local_fetch ($node) {
         my $node_type = $node->get_type;
 
@@ -69,28 +122,6 @@ class B::MOP::Tools::PropogateTypeInformation {
         else {
             die "TYPE ERROR: Cannot cast $node_type to $target_type"
                 unless $node_type->is_same_type($target_type);
-        }
-    }
-
-    method visit_numeric_op ($node) {
-        my $node_type = $node->get_type;
-        my $lhs_type  = $node->lhs->get_type;
-        my $rhs_type  = $node->rhs->get_type;
-
-        say ">> node($node_type) = lhs($lhs_type) <op> rhs($rhs_type)";
-
-        if ($lhs_type->is_same_type($rhs_type)) {
-            say ">> lhs($lhs_type) == rhs($rhs_type)";
-            if (my $new_type = $node_type->cast($lhs_type)) {
-                say ">> new($new_type) for $node";
-                $node->set_type($new_type);
-            }
-            else {
-                say ">> could not cast $node_type to lhs($lhs_type) for $node";
-            }
-        }
-        else {
-            say ">> lhs($lhs_type) != rhs($rhs_type)";
         }
     }
 }
---
+++ t/005-more-numeric-ops.t
@@ -0,0 +1,48 @@
+#!perl
+
+use v5.40;
+use experimental qw[ class ];
+
+use YAML qw[ Dump ];
+use Test::More;
+
+use B::MOP;
+
+package Foo {
+    sub test {
+        my $x;
+        my $y = ($x + $x) + 12.5;
+    }
+}
+
+my $Foo = B::MOP->new->load_package('Foo');
+isa_ok($Foo,  'B::MOP::Package');
+
+subtest '... Foo::test' => sub {
+    my $test = $Foo->get_subroutine('test');
+    isa_ok($test, 'B::MOP::Subroutine');
+
+    subtest '... testing the pad' => sub {
+        my ($x, $y) = $test->pad_variables;
+        isa_ok($x, 'B::MOP::Variable');
+        is($x->name, '$x', '... got the expected name for $x');
+        isa_ok($x->get_type, 'B::MOP::Type::Scalar');
+
+        isa_ok($y, 'B::MOP::Variable');
+        is($y->name, '$y', '... got the expected name for $y');
+        isa_ok($y->get_type, 'B::MOP::Type::Numeric');
+    };
+
+    subtest '... testing the AST' => sub {
+        my $ast  = $test->ast;
+        isa_ok($ast,  'B::MOP::AST::Subroutine');
+
+        my $block = $ast->block;
+        isa_ok($block, 'B::MOP::AST::Block');
+    };
+
+    say Dump $test->ast->to_JSON if $ENV{DEBUG};
+};
+
+
+done_testing;
